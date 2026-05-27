a1aca8d633ba11aae72a7781db2841eb1d50024e
alinemonteirocampelo <aline.campelo.monteiro@gmail.com>
2026-05-27 16:07:25 -0300

    algoritmos-e-vetores

---
+++ ex-extras/ex.01/script.js
@@ -0,0 +1,19 @@
+// Desenvolva um algoritmo que mostre uma contagem regressiva de 30 até 1, marcando os números que forem divisíveis por 4, exatamente como mostrado abaixo:
+// 30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16]...
+
+let resultado = "";
+
+for (let i = 30; i >= 1; i--) {
+    
+    if (i % 4 == 0) {
+        resultado = resultado + "[" + i + "]";
+    } else {
+        resultado = resultado + i;
+    }
+
+    if (i > 1) {
+        resultado = resultado + " ";
+    }
+}
+
+alert(resultado);
